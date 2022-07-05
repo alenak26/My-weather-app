d39304b212f11d76a8b70d9a23f0b0178a1455f5
alenak26 <108095300+alenak26@users.noreply.github.com>
2022-07-05 17:34:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "958974901cf1ec32303b593f9d980eaa";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
